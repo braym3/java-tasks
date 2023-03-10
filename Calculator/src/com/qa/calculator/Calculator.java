c675c9f8881372abcb6150558e959f67b7e4b4f6
braym3 <126671044+braym3@users.noreply.github.com>
2023-03-10 10:48:59 +0000

    Added conditional on division

--- Calculator/src/com/qa/calculator/Calculator.java
+++ Calculator/src/com/qa/calculator/Calculator.java
@@ -15,7 +15,12 @@ public class Calculator {
 	}
 
 	public static double divide(double num1, double num2) {
-		return num1 / num2;
+		if (num1 > num2) {
+			return num1 / num2;
+		} else {
+			System.out.printf("The division cannot be performed. " + num1 + " is smaller than " + num2);
+			return 0;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -30,6 +35,12 @@ public class Calculator {
 
 		// Divide 284 and 33
 		System.out.println(divide(284, 33));
+		
+		// Divide 11 by 62
+		double result = divide(11, 62);
+		if (result != 0) {
+			System.out.println(divide(11, 62));
+		}
 	}
 
 }
